8287d59bcc9c4de55450231a770a7830f6072038
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-20 19:08:16 -0300

    Reenviando pq não passou no github

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -8,7 +8,6 @@ loginRouter.post('/', (req, res) => {
         const { email, password } = req.body;
         const token = randomUUID().replace(/-/g, '').substring(0, 16);
         if (email && password) return res.status(200).json({ token });
-        return res.status(404).end();
     } catch (error) {
         return res.status(400).end();
     }
