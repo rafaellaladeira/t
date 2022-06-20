00d55f67c235635a5e99d0015d3de26b02daee01
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-20 19:48:10 -0300

    Fix

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -5,7 +5,8 @@ const loginRouter = Router();
 
 loginRouter.post('/', (req, res) => {
     const { email, password } = req.body;
-    const token = randomUUID().replace(/-/g, '').substring(0, 16);
+    const toke = randomUUID();
+    const token = toke.replace(/-/g, '').substring(0, 16);
     if (email && password) res.status(200).json({ token });
 });
 
