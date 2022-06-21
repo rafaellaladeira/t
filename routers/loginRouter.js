c367caf9d424335f0350bbedb4dca2e26835b178
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-21 09:14:13 -0300

    Fix: Github não aceita randomUUID

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
-const { randomUUID } = require('crypto');
 
 const loginRouter = Router();
 
 loginRouter.post('/', (req, res) => {
     const { email, password } = req.body;
-    const toke = randomUUID();
-    const token = toke.replace(/-/g, '').substring(0, 16);
+    const token = Math.random().toString(14);
     if (email && password) res.status(200).json({ token });
 });
 
-module.exports = loginRouter;
+module.exports = loginRouter; 
+
+// const token = toke.replace(/-/g, '').substring(0, 16);
